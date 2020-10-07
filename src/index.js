84cfb3aadb0238a29d54780d9602d5a1cdec941b
Vladimir Sabev <vdsabev@gmail.com>
2020-10-07 12:37:58 +0300

    Import client modules directly from unpkg

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import html from './html.js'
+import { html, render } from './dom.js'
 
 const documentApiUrl = '/api/document'
 const getDocumentId = () => new URLSearchParams(location.search).get('id')
 
-/** @type {(props: { id: string }) => HTMLIFrameElement}} */
-const Document = ({ id }) => html`
+/** @type {(props: { id: string, makeImagesResponsive?: boolean }) => HTMLIFrameElement}} */
+const Document = ({ id, makeImagesResponsive }) => html`
   <iframe
     src="${documentApiUrl}/${id}"
     scrolling="no"
     onLoad=${(e) => {
       /** @type {HTMLIFrameElement} */ const iframe = e.target
-      iframe.style.height = `${iframe.contentWindow.document.body.offsetHeight}px`
+      const iframeDocument = iframe.contentWindow.document
+      iframe.style.height = `${iframeDocument.body.offsetHeight}px`
+
+      if (makeImagesResponsive) {
+        Array.from(iframeDocument.querySelectorAll('img')).forEach((img) => {
+          img.style.maxWidth = '100%'
+          img.style.width = img.parentNode.style.width = 'auto'
+          img.style.height = img.parentNode.style.height = 'auto'
+        })
+      }
     }}
   />
 `
 
-document.querySelector('main').appendChild(Document({ id: getDocumentId() }))
+render(
+  html`<${Document} id="${getDocumentId()}" makeImagesResponsive />`,
+  document.querySelector('main')
+)
