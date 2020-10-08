0ea0ec41b5e872873372ec11a83e318a7e137f2d
Vladimir Sabev <vdsabev@gmail.com>
2020-10-08 16:33:44 +0300

    Refactor with useState

--- public/dom.js
+++ public/dom.js
@@ -1,1 +1,18 @@
-export { html, render, Component } from 'https://unpkg.com/htm@3.0.4/preact/standalone.module.js'
+// https://github.com/developit/htm/blob/c1339af059c5299ca524586103c6cae0f85c8211/src/integrations/preact/standalone.mjs
+export {
+  // h,
+  html,
+  render,
+  // Component,
+  createContext,
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue,
+} from 'https://unpkg.com/htm@3.0.4/preact/standalone.module.js'
--- public/index.js
+++ public/index.js
@@ -1,40 +1,34 @@
-import { html, render, Component } from './dom.js'
+import { html, render, useState } from './dom.js'
 
 const documentApiUrl = '/api/document'
 const getDocumentId = () => new URLSearchParams(location.search).get('id')
 
 /** @type {(props: { id: string, makeImagesResponsive?: boolean }) => HTMLIFrameElement}} */
-class Document extends Component {
-  render({ id, makeImagesResponsive }) {
-    const { documentHeight } = this.state
-    const setDocumentHeight = (documentHeight) => {
-      this.setState({ documentHeight })
-    }
+const Document = ({ id, makeImagesResponsive, ...props }) => {
+  const [documentHeight, setDocumentHeight] = useState(null)
 
-    return html`
-      <iframe
-        src="${documentApiUrl}/${id}"
-        scrolling="no"
-        style=${documentHeight == null
-          ? { opacity: 0 }
-          : { animation: 'fadeIn 600ms', height: `${documentHeight}px` }}
-        onLoad=${(e) => {
-          /** @type {HTMLIFrameElement} */ const iframe = e.target
-          const iframeDocument = iframe.contentWindow.document
-          if (makeImagesResponsive) {
-            Array.from(iframeDocument.querySelectorAll('img')).forEach(
-              (img) => {
-                img.style.maxWidth = '100%'
-                img.style.width = img.parentNode.style.width = 'auto'
-                img.style.height = img.parentNode.style.height = 'auto'
-              }
-            )
-          }
-          setDocumentHeight(iframeDocument.body.offsetHeight)
-        }}
-      />
-    `
-  }
+  return html`
+    <iframe
+      scrolling="no"
+      ...${props}
+      src="${documentApiUrl}/${id}"
+      style=${documentHeight == null
+        ? { opacity: 0 }
+        : { animation: 'fadeIn 600ms', height: `${documentHeight}px` }}
+      onLoad=${(e) => {
+        /** @type {HTMLIFrameElement} */ const iframe = e.target
+        const iframeDocument = iframe.contentWindow.document
+        if (makeImagesResponsive) {
+          Array.from(iframeDocument.querySelectorAll('img')).forEach((img) => {
+            img.style.maxWidth = '100%'
+            img.style.width = img.parentNode.style.width = 'auto'
+            img.style.height = img.parentNode.style.height = 'auto'
+          })
+        }
+        setDocumentHeight(iframeDocument.body.offsetHeight)
+      }}
+    />
+  `
 }
 
 render(
